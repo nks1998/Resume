d63b6129de3c24fc1e139d9da6965e11f1e7e472
Nihal Kumar Sharma <nks393790@gmail.com>
2021-07-30 15:43:58 +0530

    Complete cv

---
+++ index.js
@@ -0,0 +1,8 @@
+var icon=document.querySelector(".icon");
+var drop=document.querySelector(".drop");
+icon.addEventListener("click",function(){
+    drop.classList.toggle("hidden");
+})
+drop.addEventListener("click",function(){
+    drop.classList.toggle("hidden");
+})
